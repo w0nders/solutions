3040a4934b6f4e1bb9f9b35b30841af667ba0dcf
w0nders <40396241+w0nders@users.noreply.github.com>
2018-06-22 11:21:09 +0300

    1
    1

---
+++ scripts/create_shadowsocks_cfg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ip=`ifconfig eth0 |grep -e 'inet addr:'|awk '{FS=":";print $2}'| sed 's/addr://g'`
+echo ="
+{
+    \"server\":"\"$ip"\",
+    \"server_port\":8388,
+    \"local_address\": "\"$ip"\",
+    \"local_port\":1080,
+    \"password\":"\"$passwd"\",
+    \"timeout\":300,
+    \"method\":\"aes-256-cfb\",
+    \"fast_open\": false,
+    \"workers\": 1
+}" > /etc/shadowsocks-libev/config.json
