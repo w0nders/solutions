9837c738e97beabbb3e580e811d87725e626205b
Alexandr Novikov <w0nders@w0nders.com>
2019-04-08 22:15:18 +0300

    add config to subscribe

--- templates/azure/cognitive-services/quickstart.py
+++ templates/azure/cognitive-services/quickstart.py
@@ -2,7 +2,7 @@ import azure.cognitiveservices.speech as speechsdk
 
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
+speech_key, service_region = "66749a434e694c7bbfff7341ca4c1d99", "westeurope"
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
 # Creates a recognizer with the given settings
@@ -12,7 +12,7 @@ print("Say something...")
 
 
 # Starts speech recognition, and returns after a single utterance is recognized. The end of a
-# single utterance is determined by listening for silence at the end or until a maximum of 15
+# single tterance is determined by listening for silence at the end or until a maximum of 15
 # seconds of audio is processed.  The task returns the recognition text as result. 
 # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
 # shot recognition like command or query. 
